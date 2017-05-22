6335e6a04f1ed76a52fe3a8a3bde476c40edf73e
Menglei Lei <mengleilei2018@u.northwestern.edu>
2017-05-22 14:57:16 -0500

    add match page

--- IMC/src/Pages/MatchPage/matchpage.ts
+++ IMC/src/Pages/MatchPage/matchpage.ts
@@ -9,48 +9,34 @@ import { AF } from '../Providers/firebase';
 export class MatchPage {
   results = [
     {
-      "name" : "Google",
-      "icon" : "glyphicon glyphicon-home"
+      "img" : './assets/img/acton.jpg',
+      'name' : 'Act On',
+      'description' : 'Act-On gives you the tools to quickly and easily set up strategic campaigns that deploy automatically based on your pre-programmed rules. From simple messages to highly sophisticated campaigns, Act-On’s automated programs allow you to use conditional “if-then” logic to trigger the next step in a campaign. You can set rules to automatically trigger emails, sync lists, and update data fields.',
+      'link' : 'www.google.com'
     },
     {
-      "name" : "Facebook",
-      "icon" : "glyphicon glyphicon-home"
+      "img" : './assets/img/contactpigeon.jpg',
+      'name' : 'Contact Pigeon',
+      'description' : 'ContactPigeon is a marketing automation platform that combines contact management, e-commerce analytics, campaign generator, automation smart-logic and reporting dashboard.',
+      'link' : 'www.google.com'
     },
     {
-      "name" : "IBM Watson",
-      "icon" : "glyphicon glyphicon-home"
+      "img" : './assets/img/salesforce.png',
+      'name' : 'Pardot',
+      'description' : 'Pardot is an on-demand software solution that provides a simple marketing automation platform to grow pipeline and generate better leads by allowing marketing and sales departments to create, deploy, automate, and manage marketing campaigns online.',
+      'link' : 'www.google.com'
     },
     {
-      "name" : "Salesforce",
-      "icon" : "glyphicon glyphicon-home"
+      "img" : './assets/img/ibm.jpg',
+      'name' : 'IBM Watson Campaign Automation',
+      'description' : 'IBM Watson Campaign Automation is a cloud-based digital marketing platform for email marketing, lead management and mobile and social engagement solutions.',
+      'link' : 'www.google.com'
     },
-    {
-      "name" : "Adobe",
-      "icon" : "glyphicon glyphicon-home"
-    }
   ];
-  mystack = [];
   constructor(public af:AF ){
-    this.af.af.auth.subscribe((data) => {
-      this.af.marketers = data ? this.af.af.database.object('marketers/' + data.uid) : null;
-      this.af.marketers.subscribe((data) => {
-        console.log(data);
-        this.mystack = data.MartechStack ? data.MartechStack : [];
-      });
-    });
+
   }
 
-  AddTech(name) : void{
-    for(var i = 0; i < this.results.length; i ++){
-      if(this.results[i].name == name){
-        let temp = this.results[i];
-        this.af.AddTech(temp);
-        this.results.splice(i, 1);
-        return ;
-      }
-    }
-    return ;
-  }
 
 
 }
